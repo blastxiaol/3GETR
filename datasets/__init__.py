413c76b942502b8fbfa93724c7568ae4051f39cf
blastxiaol <blastxiaol@gmail.com>
2022-09-18 07:46:04 +0000

    init

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,34 +1,5 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-from .scannet import ScannetDetectionDataset, ScannetDatasetConfig
-from .sunrgbd import SunrgbdDetectionDataset, SunrgbdDatasetConfig
+from .sunrefer import SUNREFER3D
 
-
-DATASET_FUNCTIONS = {
-    "scannet": [ScannetDetectionDataset, ScannetDatasetConfig],
-    "sunrgbd": [SunrgbdDetectionDataset, SunrgbdDatasetConfig],
-}
-
-
-def build_dataset(args):
-    dataset_builder = DATASET_FUNCTIONS[args.dataset_name][0]
-    dataset_config = DATASET_FUNCTIONS[args.dataset_name][1]()
-    
-    dataset_dict = {
-        "train": dataset_builder(
-            dataset_config, 
-            split_set="train", 
-            root_dir=args.dataset_root_dir, 
-            meta_data_dir=args.meta_data_dir, 
-            use_color=args.use_color,
-            augment=True
-        ),
-        "test": dataset_builder(
-            dataset_config, 
-            split_set="val", 
-            root_dir=args.dataset_root_dir, 
-            use_color=args.use_color,
-            augment=False
-        ),
-    }
-    return dataset_dict, dataset_config
+def build_dataset(args, isTrain):
+    return SUNREFER3D(args, isTrain)
     
